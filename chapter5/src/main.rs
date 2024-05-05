7ac87bacd9ffd832754fa6f5b9b306bacaf3d328
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-05-05 19:21:26 -0400

    ch5 complete

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -20,3 +20,26 @@ fn main(){
     // to get a specfic value we use the dot notation
     let user1 = User.username
 }
+
+
+// we can create new instances of structs by using the struct name and then the values for each field in curly brackets
+
+struct User{
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+fn mn(){
+    let person1 = User {
+        email: String::from("someone@gmail.com"),
+        username: String::from("someone"),
+        active: true,
+        sign_in_count: 1,
+    }
+
+}
+
+
+// Tuple structs are useful when the fields of the struct do not require named access, 
+// and the order and type of the fields is the primary semantic information.
