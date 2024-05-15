fa863416af4e953ff787d2d1c2727f30e45443db
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-05-15 10:15:41 -0400

    first rust challenege

---
+++ rust_excersises/stringReversed/src/main.rs
@@ -0,0 +1,24 @@
+fn reverse_string(s: &str) -> String {
+    //expresssion
+    s.chars().rev().collect()
+}
+
+fn main() {
+    let name = String::from("Seif");
+    let last_name = String::from("Mamdouh");
+
+    let reversed_name = reverse_string(&name);
+
+    let full_name = reversed_name + " " + &last_name;
+
+    println!("The full name is {}", full_name);
+}
+
+
+
+
+// lessons learned
+// String literals are immutable and they stored in a binary file
+// to make string mutable you have to use let mut var = String::from("string");
+
+//expressions evaluate the value but doesn't return anything also they dont't end in semicolon
---
+++ rust_excersises/stringReversed/target/debug/stringReversed.d
@@ -0,0 +1,1 @@
+/Users/seifmamdouh/Rust-projects/hello_world/rust_excersises/stringReversed/target/debug/stringReversed: /Users/seifmamdouh/Rust-projects/hello_world/rust_excersises/stringReversed/src/main.rs
